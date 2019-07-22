c093a0d5ebc9381ad1c80d163d2a3378753f558c
Yongyan-Jiang <2810312487@qq.com>
2019-07-22 16:14:39 +0800

    build

---
+++ build/precache-manifest.813cf2765005faed578822fd3b7203d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4459435a2768f08d506586d7242715",
+    "url": "/todo-list/build/index.html"
+  },
+  {
+    "revision": "1460d894f08e1458ae13",
+    "url": "/todo-list/build/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/todo-list/build/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1460d894f08e1458ae13",
+    "url": "/todo-list/build/static/js/main.7c1ce663.chunk.js"
+  },
+  {
+    "revision": "14419199280afec42081",
+    "url": "/todo-list/build/static/js/runtime~main.12b73f89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
