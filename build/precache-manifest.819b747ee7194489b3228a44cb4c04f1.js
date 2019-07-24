a4c5d72394d32ee35747636335533a1725a3242e
Yongyan-Jiang <2810312487@qq.com>
2019-07-24 14:22:56 +0800

    把每个待办事项封装成TodoItem

--- build/precache-manifest.819b747ee7194489b3228a44cb4c04f1.js
+++ build/precache-manifest.819b747ee7194489b3228a44cb4c04f1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17f541703e67d3e00724eaa87e85fd22",
+    "revision": "14e58996ca4b922bc60d507929876478",
     "url": "/todo-list/build/index.html"
   },
   {
-    "revision": "3e0aad97bc3bcce2b932",
+    "revision": "39f05aac187670208c03",
     "url": "/todo-list/build/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/todo-list/build/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "3e0aad97bc3bcce2b932",
-    "url": "/todo-list/build/static/js/main.ab3dc00e.chunk.js"
+    "revision": "39f05aac187670208c03",
+    "url": "/todo-list/build/static/js/main.ea6c8133.chunk.js"
   },
   {
     "revision": "14419199280afec42081",
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
 class App extends Component {
   constructor(props){
@@ -8,13 +9,18 @@ class App extends Component {
     this.state = {
       newTodo: 'test',
       todoList: [
-        {id:1, title:'第一个待办'}
+        {id:1, title:'第一个待办'},
+        {id:2, title:'第二个待办'},
       ]
     }
   }
   render() {
     let todos = this.state.todoList.map((item,index)=>{
-      return <li>{item.title}</li>
+      return ( //必须要有括号
+        <li>
+          <TodoItem todo={item} />
+        </li>
+      )
     })
     return (
       <div className="App">
