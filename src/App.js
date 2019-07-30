2909ddea73d85be418a99160374d48892c3b28da
Yongyan-Jiang <2810312487@qq.com>
2019-07-30 18:08:37 +0800

    删除 localStore 功能

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './reset.css'
 import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
-import * as localStore from './localStore'
 
 
 class App extends Component {
@@ -12,7 +11,7 @@ class App extends Component {
     super(props)
     this.state = {
       newTodo: 'test',
-      todoList: localStore.load('todoList') || []
+      todoList: []
     }
   }
   render() {
@@ -43,7 +42,7 @@ class App extends Component {
     )
   }
   componentDidUpdate(){
-    localStore.save('todoList', this.state.todoList)
+    
   }
   toggle(e, todo){
     todo.status = todo.status === 'completed' ? '' : 'completed'
