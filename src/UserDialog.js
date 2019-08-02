24b0a96b8f8d5bde6573376ac865a57e5084a40f
Yongyan-Jiang <2810312487@qq.com>
2019-08-02 16:37:50 +0800

    添加忘记密码按钮，并需要在注册时提供邮箱，以便找回密码

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -8,6 +8,7 @@ export default class UserDialog extends Component{
     this.state = {
       selected: 'signUp',
       formData: {
+        email: '',
         username: '',
         password: '',
       }
@@ -21,7 +22,7 @@ export default class UserDialog extends Component{
 
   signUp(e){
     e.preventDefault()
-    let {username, password} = this.state.formData
+    let {email,username, password} = this.state.formData
     let success = (user)=>{
       this.props.onSignUp.call(null, user)
     }
@@ -35,7 +36,7 @@ export default class UserDialog extends Component{
           break
       }
     }
-    signUp(username, password, success, error)
+    signUp(email,username, password, success, error)
   }
   signIn(e){
     e.preventDefault()
@@ -66,6 +67,11 @@ export default class UserDialog extends Component{
     let signUpForm = (
       <form className="signUp" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
         <div className="row">
+          <label>邮箱</label> 
+          <input type="text" value={this.state.formData.email}
+            onChange={this.changeFormData.bind(this, 'email')}/>
+        </div>
+        <div className="row">
           <label>用户名</label> 
           <input type="text" value={this.state.formData.username} 
             onChange={this.changeFormData.bind(this, 'username')}/>
@@ -95,6 +101,7 @@ export default class UserDialog extends Component{
         </div>
         <div className="row actions">
           <button type="submit">登录</button>
+          <a href="javascript:;">忘记密码了？</a>
         </div>
       </form>
     ) 
